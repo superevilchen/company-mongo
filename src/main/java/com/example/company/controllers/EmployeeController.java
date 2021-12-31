21116ef2ede35bb8badf675ca5dd42c501c6c9b4
chen var <varsanochen@gmail.com>
2021-12-31 11:43:28 +0200

    added controller

--- src/main/java/com/example/company/controllers/EmployeeController.java
+++ src/main/java/com/example/company/controllers/EmployeeController.java
@@ -1,9 +1,42 @@
 package com.example.company.controllers;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.example.company.beans.Employee;
+import com.example.company.services.EmployeeService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
-@RequestMapping("/api/v1/company")
+@RequestMapping(EmployeeController.BASE_URL)
+@RequiredArgsConstructor
 public class EmployeeController {
+
+    protected static final String BASE_URL = "/api/v1/company";
+    private final EmployeeService employeeService;
+
+    @GetMapping("/employees")
+    public List<Employee> getAllEmployees(){
+        return employeeService.getEmployees();
+    }
+
+    @GetMapping("/{id}")
+    public Employee getOneEmployee(@PathVariable String id) throws Exception {
+        return employeeService.getOneEmployee(id);
+    }
+
+    @PostMapping("/add")
+    public void addEmployee(@RequestBody Employee e){
+        employeeService.addEmployee(e);
+    }
+
+    @PutMapping("update/{id}")
+    public void updateEmployee(@PathVariable String id, @RequestBody Employee e) throws Exception {
+        employeeService.updateEmployee(id, e);
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public void deleteEmployee(@PathVariable String id) throws Exception {
+        employeeService.deleteEmployee(id);
+    }
 }
